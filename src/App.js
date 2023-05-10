5c5d68e6636b552bd0f4da26ad729c6e5eab18bb
ToDy95 <todyimmortal@gmail.com>
2023-05-10 19:47:23 +0300

    filter bar and filter functionaly

--- src/App.js
+++ src/App.js
@@ -1,69 +1,75 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from './components/Card';
 import CreateTask from './components/CreateTask';
 import Footer from './components/Footer';
 
 function App() {
   // const [count, setCount] = useState(0);
-  const [data, setData] = useState([
-    {
-      title: 'Task 1',
-      status: 'In progress',
-      description: 'Bla bla bla',
-      dueDate: new Date('12 24, 22'),
-    },
-    {
-      title: 'Task 2',
-      status: 'In progress',
-      description: 'Bla bla bla',
-      dueDate: new Date('12 24, 22'),
-    },
-    {
-      title: 'Task 3',
-      status: 'In progress',
-      description: 'Bla bla bla',
-      dueDate: new Date('12 24, 22'),
-    },
-    {
-      title: 'Task 4',
-      status: 'In progress',
-      description: 'Bla bla bla',
-      dueDate: new Date('12 24, 22'),
-    },
-    {
-      title: 'Task 5',
-      status: 'In progress',
-      description: 'Bla bla bla',
-      dueDate: new Date('09 14, 22'),
-    },
-    {
-      title: 'Task 6',
-      status: 'In progress',
-      description: 'Bla bla bla',
-      dueDate: new Date('12 24, 22'),
-    },
-  ]);
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  const [filterState, setFilterState] = useState([]);
+
+  async function getData() {
+    const res = await fetch('./data.json');
+    const response = await res.json();
+    setData(response);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setFilterState(
+      data.filter(item =>
+        item.title.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search.length > 3, data]);
 
   const addNewTask = newTask => {
     setData(oldData => [...oldData, newTask]);
   };
   return (
     <div className="main">
+      <input
+        type="search"
+        value={search}
+        name="search"
+        onChange={e => setSearch(e.target.value)}
+      />
+
       <div className="container">
         <div className="cardList">
-          {data.map((item, index) => {
-            return (
-              <Card
-                key={index}
-                title={item.title}
-                status={item.status}
-                description={item.description}
-                dueDate={item.dueDate}
-              />
-            );
-          })}
+          {filterState.length > 0 ? (
+            filterState.map((item, index) => {
+              return (
+                <Card
+                  key={index}
+                  title={item.title}
+                  status={item.status}
+                  description={item.description}
+                  dueDate={new Date(item.dueDate)}
+                />
+              );
+            })
+          ) : filterState.length === 0 && search.length > 0 ? (
+            <h1>Nothing was found !!!</h1>
+          ) : (
+            data.map((item, index) => {
+              return (
+                <Card
+                  key={index}
+                  title={item.title}
+                  status={item.status}
+                  description={item.description}
+                  dueDate={new Date(item.dueDate)}
+                />
+              );
+            })
+          )}
         </div>
         <CreateTask addNewTask={addNewTask} />
       </div>
